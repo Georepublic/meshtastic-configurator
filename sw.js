7a2af994da48235add47b327ad2a0ca6458e6a52
dkastl <dkastl@users.noreply.github.com>
2024-10-04 03:38:52 +0000

    deploy: 64a16a990e584e995bc164d13f7bde3aaddde74c

--- sw.js
+++ sw.js
@@ -2,15 +2,27 @@ const CACHE_NAME = "meshtastic-cache";
 const urlsToCache = [
   "/meshtastic-configurator/",
   "/meshtastic-configurator/index.html",
-  "/meshtastic-configurator/src/styles.css",
-  "/meshtastic-configurator/src/main.ts",
   "/meshtastic-configurator/assets/icon-192x192.png",
-  "/meshtastic-configurator/assets/icon-512x512.png"
+  "/meshtastic-configurator/assets/icon-512x512.png",
+  // Files will be dynamically handled
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then(async cache => {
+      // Open cache and fetch the manifest for hashed files
+      const manifest = await fetch("/meshtastic-configurator/manifest.json").then(response => response.json());
+
+      // Add all hashed files (CSS/JS) to the cache
+      const urlsToCache = [
+        "/meshtastic-configurator/",
+        "/meshtastic-configurator/index.html",
+        `/meshtastic-configurator/${manifest['src/main.ts'].file}`,
+        `/meshtastic-configurator/${manifest['src/styles.css'].file}`,
+        "/meshtastic-configurator/assets/icon-192x192.png",
+        "/meshtastic-configurator/assets/icon-512x512.png"
+      ];
+
       return cache.addAll(urlsToCache);
     })
   );
